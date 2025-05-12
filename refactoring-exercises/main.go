de006265482ea652778d08075f9555a9b0129c87
Cezar Augusto <cezar804@gmail.com>
2025-05-12 23:48:49 +0900

    fix: adding id to DELETE endpoints

--- refactoring-exercises/main.go
+++ refactoring-exercises/main.go
@@ -20,15 +20,15 @@ func main() {
 	r.HandleFunc("/api/itens/codigo/{codigo}", handlers.GetItemByCodigo).Methods(http.MethodGet) // get-code?codigo=TEC001
 	r.HandleFunc("/api/itens", handlers.CreateItem).Methods(http.MethodPost)                     // POST para criar um item
 	r.HandleFunc("/api/itens", handlers.UpdateItem).Methods(http.MethodPut)                      // PUT para atualizar um item (JSON com id)
-	r.HandleFunc("/api/itens", handlers.DeleteItem).Methods(http.MethodDelete)                   // DELETE para deletar um item (espera id via query: ?id=1)
+	r.HandleFunc("/api/itens/{id}", handlers.DeleteItem).Methods(http.MethodDelete)              // DELETE para deletar um item (espera id via query: ?id=1)
 
 	// Endpoints para Categorias
 	const baseCategoriasApi = "/api/categorias"
-	r.HandleFunc(baseCategoriasApi, handlers.ListCategorias).Methods(http.MethodGet)       // GET para listar todas as categorias
-	r.HandleFunc(baseCategoriasApi+"/{id}", handlers.GetCategoria).Methods(http.MethodGet) // GET para buscar uma categoria (espera id via query)
-	r.HandleFunc(baseCategoriasApi, handlers.CreateCategoria).Methods(http.MethodPost)     // POST para criar uma categoria
-	r.HandleFunc(baseCategoriasApi, handlers.UpdateCategoria).Methods(http.MethodPut)      // PUT para atualizar uma categoria (JSON com id)
-	r.HandleFunc(baseCategoriasApi, handlers.DeleteCategoria).Methods(http.MethodDelete)   // DELETE para deletar uma categoria (espera id via query)
+	r.HandleFunc(baseCategoriasApi, handlers.ListCategorias).Methods(http.MethodGet)             // GET para listar todas as categorias
+	r.HandleFunc(baseCategoriasApi+"/{id}", handlers.GetCategoria).Methods(http.MethodGet)       // GET para buscar uma categoria (espera id via query)
+	r.HandleFunc(baseCategoriasApi, handlers.CreateCategoria).Methods(http.MethodPost)           // POST para criar uma categoria
+	r.HandleFunc(baseCategoriasApi, handlers.UpdateCategoria).Methods(http.MethodPut)            // PUT para atualizar uma categoria (JSON com id)
+	r.HandleFunc(baseCategoriasApi+"/{id}", handlers.DeleteCategoria).Methods(http.MethodDelete) // DELETE para deletar uma categoria (espera id via query)
 
 	log.Println("Servidor rodando na porta 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
